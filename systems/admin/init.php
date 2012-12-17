7a3347097e677c00ab38071c83923f82751a215c
Chris Smith <dmagick@gmail.com>
2012-12-18 09:09:35 +1100

    set the template dir to the right path when in the admin area

--- systems/admin/init.php
+++ systems/admin/init.php
@@ -9,11 +9,13 @@ $systems = array(
     'user',
 );
 
+// Use different templates for the admin area.
+template::setDir($basedir.'/templates/admin', 'template');
+
 // The admin area will always use a session to see if a visitor
 // is logged in or not, so we'll start it up now.
 session::start();
 
 // We also always need the user system.
 loadSystem('user');
-
 
