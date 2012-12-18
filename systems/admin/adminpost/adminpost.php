1ef94519460953387198147852874c799a18f083
Chris Smith <dmagick@gmail.com>
2012-12-18 14:55:51 +1100

    get posts updated through ajax calls. also fix the get posts list to include the content as well.

--- systems/admin/adminpost/adminpost.php
+++ systems/admin/adminpost/adminpost.php
@@ -26,8 +26,25 @@ class adminpost
     public static function process($action='')
     {
 
+        if (strpos($action, '/') !== FALSE) {
+            $bits   = explode('/', $action);
+            $action = array_shift($bits);
+            $info   = implode('/', $bits);
+        }
+
         switch ($action)
         {
+            case 'update':
+                template::clearStack();
+                $result = self::_updatePost($info);
+                if ($result === TRUE) {
+                    echo 'Post updated';
+                } else {
+                    echo 'Something went wrong';
+                }
+                exit;
+                break;
+
             case '':
                 self::_listPosts();
             break;
@@ -35,20 +52,117 @@ class adminpost
         }
     }
 
+    private static function _updatePost($postid)
+    {
+        db::beginTransaction();
+
+        $status = $_POST['status'];
+        $content = $_POST['content'];
+        $subject = $_POST['subject'];
+
+        $sqlUpdate  = 'UPDATE ~tablename~ SET';
+        $sqlUpdate .= ' subject=:subject,';
+        $sqlUpdate .= ' content=:content,';
+        $sqlUpdate .= ' modifieddate=NOW()';
+        $sqlUpdate .= ' WHERE ';
+        $sqlUpdate .= ' postid=:postid';
+
+        $updateData = array(
+            ':subject' => $subject,
+            ':content' => $content,
+            ':postid'  => $postid,
+        );
+
+        $sqlInsert  = 'INSERT INTO ~tablename~';
+        $sqlInsert .= '(postid, subject, content, postdate, modifieddate, postby)';
+        $sqlInsert .= ' SELECT postid, :subject, :content, postdate, NOW(), postby ';
+        $sqlInsert .= ' FROM ~othertablename~';
+        $sqlInsert .= ' WHERE postid=:postid';
+
+        $insertData = array(
+            ':subject' => $subject,
+            ':content' => $content,
+            ':postid'  => $postid,
+        );
+
+        $sqlDelete  = 'DELETE FROM ~tablename~ WHERE postid=:postid';
+
+        $deleteData = array(
+            ':postid' => $postid,
+        );
+
+        switch ($status)
+        {
+            case 'live':
+                $updateTable  = 'posts';
+                $insertTable1 = 'posts';
+                $insertTable2 = 'posts_queue';
+                $deleteTable  = 'posts_queue';
+                break;
+
+            case 'uc':
+                $updateTable  = 'posts_queue';
+                $insertTable1 = 'posts_queue';
+                $insertTable2 = 'posts';
+                $deleteTable  = 'posts';
+                break;
+        }
+
+        $updated = db::execute(
+            str_replace('~tablename~', db::getPrefix().$updateTable, $sqlUpdate),
+            $updateData
+        );
+
+        if ($updated == 0) {
+            $inserted = db::execute(
+                str_replace(
+                    array(
+                        '~tablename~',
+                        '~othertablename~',
+                    ),
+                    array(
+                        db::getPrefix().$insertTable1,
+                        db::getPrefix().$insertTable2,
+                    ),
+                    $sqlInsert
+                ),
+                $insertData
+            );
+            if ($inserted == 0) {
+                messageLog::LogMessage('Unable to move from '.$insertTable1.' to '.$insertTable2);
+                db::rollbackTransaction();
+                return FALSE;
+            }
+
+            $deleted = db::execute(
+                str_replace('~tablename~', db::getPrefix().$deleteTable, $sqlDelete),
+                $deleteData
+            );
+
+            if ($deleted == 0) {
+                messageLog::LogMessage('Unable to delete from '.$deleteTable);
+                db::rollbackTransaction();
+                return FALSE;
+            }
+        }
+        db::commitTransaction();
+        return TRUE;
+    }
+
     private static function _getPosts($limit=10)
     {
-        $sql  = "SELECT postid, subject, postdate, postedby, status";
+        $sql  = "SELECT postid, subject, content, postdate, postedby, status";
         $sql .= " FROM ";
         $sql .= "(";
         $sql .= "(";
-        $sql .= " SELECT p.postid, p.subject, p.postdate, u.username AS postedby, 'live' as status";
+        $sql .= " SELECT p.postid, p.subject, p.content, p.postdate, u.username AS postedby, 'live' as status";
         $sql .= " FROM ".db::getPrefix()."posts p INNER JOIN ".db::getPrefix()."users u";
         $sql .= " ON (p.postby=u.userid)";
         $sql .= " ORDER BY postdate DESC LIMIT ".$limit;
         $sql .= ")";
         $sql .= " UNION ALL ";
         $sql .= "(";
-        $sql .= "SELECT q.postid, q.subject, q.postdate, u.username AS postedby, 'uc' as status";
+        $sql .= "SELECT q.postid, q.subject, q.content, q.postdate, u.username AS postedby, 'uc' as status";
         $sql .= " FROM ".db::getPrefix()."posts_queue q INNER JOIN ".db::getPrefix()."users u";
         $sql .= " ON (q.postby=u.userid)";
         $sql .= " ORDER BY postdate DESC LIMIT ".$limit;
@@ -76,12 +190,26 @@ class adminpost
         template::serveTemplate('post.list.header');
         foreach ($list as $k => $details) {
             $details['postdate'] = niceDate($details['postdate']);
+            $details['content']  = htmlspecialchars($details['content']);
+
+            $details['livechecked'] = '';
+            $details['ucchecked']   = '';
+            if ($details['status'] === 'uc') {
+                $details['ucchecked'] = ' CHECKED';
+            }
+            if ($details['status'] === 'live') {
+                $details['livechecked'] = ' CHECKED';
+            }
+
             $keywords = array(
                 'postid',
                 'postedby',
                 'postdate',
                 'status',
                 'subject',
+                'content',
+                'livechecked',
+                'ucchecked',
             );
 
             foreach ($keywords as $keyword) {
